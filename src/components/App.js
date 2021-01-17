93db7075f2023ec0bf3ba41db9b5eb5eb010cac8
Kara Daviduik <kdaviduik@gmail.com>
2021-01-17 12:57:29 -0500

    added nomination feature

--- src/components/App.js
+++ src/components/App.js
@@ -8,32 +8,24 @@ const APIKey = "3b047afd";
 
 
 
-
-
-// var JSON = '{"Title":"Batman","Year":"1989","Rated":"PG-13","Released":"23 Jun 1989","Runtime":"126 min","Genre":"Action, Adventure","Director":"Tim Burton","Writer":"Bob Kane (Batman characters), Sam Hamm (story), Sam Hamm (screenplay), Warren Skaaren (screenplay)","Actors":"Michael Keaton, Jack Nicholson, Kim Basinger, Robert Wuhl","Plot":"The Dark Knight of Gotham City begins his war on crime with his first major enemy being Jack Napier, a criminal who becomes the clownishly homicidal Joker.","Language":"English, French, Spanish","Country":"USA, UK","Awards":"Won 1 Oscar. Another 7 wins & 26 nominations.","Poster":"https://m.media-amazon.com/images/M/MV5BMTYwNjAyODIyMF5BMl5BanBnXkFtZTYwNDMwMDk2._V1_SX300.jpg","Ratings":[{"Source":"Internet Movie Database","Value":"7.5/10"},{"Source":"Rotten Tomatoes","Value":"71%"},{"Source":"Metacritic","Value":"69/100"}],"Metascore":"69","imdbRating":"7.5","imdbVotes":"337,623","imdbID":"tt0096895","Type":"movie","DVD":"N/A","BoxOffice":"$251,348,343","Production":"Warner Brothers, PolyGram Filmed Entertainment, Guber-Peters Company","Website":"N/A","Response":"True"}';
-// var batman = JSON.parse(JSON); 
-// var batman = "batman";
-// const arr = [{ batman }];
-
-const arr = ["batman", "spiderman", "test"];
-
 // const movieList = arr.map((val) => (<li><Movie movie={val} /></li>));
 
-const Movie = ({ movie }) => {
-    return (
-        <div className="Movie">
-            <p>{movie.Title} ({movie.Year}) <button>Nominate</button></p>
-        </div>
-    )
-}
 
-function MyList(props) {
-    const arr = props.data;
-    const listItems = arr.map((val, index) =>
-      <li key={index}><Movie movie={val}/></li>
-    );
-    return <ul>{listItems}</ul>;
-  }
+
+
+
+
+
+// array nominations list, array of {movie}s
+// display nominations list
+// button onClick calls nominate function
+// nominate: checks if movie is already in array, if not then display it
+
+
+
+
+
+
 
 
 
@@ -45,17 +37,92 @@ const App = () => {
     const [movieResults, setMovieResults] = useState([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
-    // const [loading, setLoading] = useState(false);
+    const [nominees, setNominees] = useState([]);
 
 
     function updateSearchQuery(e) {
         setSearchQuery(e.target.value);
     }
-    // const updateSearchQuery = (e) => {
-    //     setSearchQuery(e.target.value);
-    // }
 
-    
+    const RemoveNomination = ({movie}) => {
+        // window.alert(JSON.stringify(nominees[0].movie.Title));
+        // window.alert(JSON.stringify({movie}.movie.Title));
+        // window.alert(JSON.stringify(nominees.filter((nominee) => nominee.movie.Title === {movie}.movie.Title)));
+        setNominees(nominees.filter((nominee) => nominee.movie.Title !== {movie}.movie.Title));
+        movie.Nominated = "false";
+
+    }
+
+    const NominateMovie = ({movie}) => {
+        // window.alert(`nominate: ${movie}`);
+        setNominees(nominees => nominees.concat({movie}));
+    }
+
+    const CheckIfNominated = ({movie}) => {
+        // value={searchQuery} onChange={updateSearchQuery}
+        if (({movie}.movie.Nominated) === "false") {
+            
+            movie.Nominated = "true";
+            // window.alert(JSON.stringify({movie}.movie));
+            NominateMovie({movie});
+            // <NominateMovie movie={movie} />
+            // NominateMovie({movie});
+        }
+        else {
+            // window.alert(JSON.stringify({movie}.movie));
+        }
+        console.log({movie});
+        // <NominateMovie movie={movie} />
+
+
+    }
+
+    const Movie = ({ movie }) => {
+        //  window.alert(JSON.stringify(Object.keys({movie}.movie)));
+        if (({movie}.movie).hasOwnProperty("Nominated")) {
+            // window.alert(`FOUND: ${JSON.stringify({movie})}`);
+            // if ({movie}.movie.Nominated) {
+            // }
+        }
+        else {
+            movie.Nominated="false";
+        }
+        
+        return (
+            // <li></li>
+            <div className="Movie">
+                <p>{movie.Title} ({movie.Year}) <button onClick={() => CheckIfNominated({movie})}>Nominate</button></p>
+            </div>
+            // movieResults.movie = 'false';
+        )
+    }
+
+    const NominatedMovie = ({movie}) => {
+        return (
+            <div className="Movie">
+                <p>{movie.Title} ({movie.Year}) <button onClick={() => RemoveNomination({movie})}>Remove</button></p>
+            </div>
+        )
+    }
+
+
+    function NominationList(props) {
+        // window.alert(JSON.stringify({nominees}));
+        const arr = props.data;
+        // window.alert(`LIST: ${props}`);
+        const listItems = arr.map((val, index) =>
+        <li key={JSON.stringify(val.movie.Title)}><NominatedMovie movie={val.movie} /></li>
+        );
+        
+        return <ul>{listItems}</ul>;
+    }
+    function MyList(props) {
+        const arr = props.data;
+        const listItems = arr.map((val, index) =>
+            <li key={JSON.stringify(val.Title)}><Movie movie={val} /></li>
+        );
+        return <ul>{listItems}</ul>;
+    }
 
 
     // }
@@ -92,40 +159,35 @@ const App = () => {
 
     const search = searchQuery => {
         // window.alert("Searching");
-
+        setMovieResults([]);
         setLoading(true);
         setError(null);
         fetch(`http://www.omdbapi.com/?s=${searchQuery}&apikey=3b047afd`)
-        .then(resp => resp)
-        .then(resp => resp.json())
-        .then(response => {
-            // window.alert(`Response: ${response}`);
-            if (response.Response === "True") {
-                // window.alert(`API: ${searchQuery}, response: ${response.Search}`);
-                // window.alert("TEST");
-                setMovieResults(response.Search);
-                // var x = JSON.stringify(movieResults);
-                
-                // window.alert(`Result: ${x}`);
-                setLoading(false);
-            } else {
-                window.alert("ERROR");
-                setError(response.Error);
-                setLoading(false);
-            }
-            
-            // window.alert({movieResults});
-            // var json = JSON.parse(movieResults);
-            // window.alert(JSON.stringify(json));
+            .then(resp => resp.json())
+            .then(response => {
+                // window.alert(`Response: ${response}`);
+                if (response.Response === "True") {
+                    // window.alert(`API: ${searchQuery}, response: ${response.Search}`);
+                    // window.alert("TEST");
+                    setMovieResults(response.Search);
+                    // var x = JSON.stringify(movieResults);
+                    // var x = JSON.stringify(movieResults[1].Title);
 
-            // console.log(movieResults);
-        })
+                    // window.alert(`Result: ${x}`);
+                    setLoading(false);
+                } else {
+                    // window.alert("ERROR");
+                    setError(response.Error);
+                    setLoading(false);
+                }
+
+                // window.alert({movieResults});
+                // var json = JSON.parse(movieResults);
+                // window.alert(JSON.stringify(json));
+
+                // console.log(movieResults);
+            })
     }
-
-    // const movieTitle = "batman";
-    // const movieYear = "1999";
-
-    // const movie = (<p>{movieTitle} ({movieYear}) <button>Nominate</button></p>);
 
 
 
@@ -138,8 +200,8 @@ const App = () => {
                     <div class="wrap">
                         <div class="search">
                             <form onSubmit={callSearch}>
-                                <input type="text" class="searchTerm" value={searchQuery} onChange={updateSearchQuery}/>
-                                <input type="submit" class="searchButton" />
+                                <input type="text" value={searchQuery} onChange={updateSearchQuery} />
+                                <input type="submit" />
                                 {/* <button type="submit" >
                                     <i class="fa fa-search"></i>
                                 </button> */}
@@ -153,47 +215,22 @@ const App = () => {
             <div class="row">
                 <div class="column">
                     <h2>Results for "{APIQuery}"</h2>
-                    
+
                     <MyList data={movieResults} />
-                    
-
-
-                    {/* <ul>
-                        movieResults.map((movie) => (
-                            <li><Movie movie={movie} /></li>
-                        ));
-
-                        {/* movieResults.map((movie) => (
-                        <li>{movie.Title} ({movie.Year}) <button>Nominate</button></li>)); */}
-                    {/* </ul>  */}
-
-                    {/* <p>Some text..</p> */}
 
                 </div>
 
 
                 <div class="column">
                     <h2>Nominations</h2>
-                    <p>Some text..</p>
+                    <NominationList data={nominees} />
+
                 </div>
 
             </div>
 
         </div>
-        //         {/* <div className="Results">
-        //             <h1>Results</h1>
-        //             <ul>
-        //                 {movieList}
-        // { 
-        //                 <li>
-        //                 {movie}
-        //                 </li> }
 
-        //             </ul>
-
-        //         </div> */
-
-        //     </div>
 
     );
 };
